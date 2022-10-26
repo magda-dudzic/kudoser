9647162671b573760f4e596a31f97e3fda051315
Magdalena Dudzic <magdalena@thecambiumgroup.co.uk>
2022-10-26 14:48:32 +0100

    Add ranking

--- app/screens/Ranking.tsx
+++ app/screens/Ranking.tsx
@@ -1,12 +1,95 @@
-import React from "react";
-import { Text } from "react-native";
+import { Ionicons } from '@expo/vector-icons';
+import { Picker } from '@react-native-picker/picker';
+import React, { useState } from 'react';
+import { FlatList, StyleSheet, Text, View } from 'react-native';
 
-import SafeArea from "../components/atoms/SafeArea";
+import SafeArea from '../components/atoms/SafeArea';
+
+const MOCK_RANKING_DATA = [
+  {
+    name: 'Magda',
+    pointsThisMonth: 120,
+    pointsInTotal: 700,
+  },
+  {
+    name: 'Sami',
+    pointsThisMonth: 130,
+    pointsInTotal: 660,
+  },
+  {
+    name: 'Philippa',
+    pointsThisMonth: 800,
+    pointsInTotal: 9900,
+  },
+  {
+    name: 'Nathan',
+    pointsThisMonth: 80,
+    pointsInTotal: 1200,
+  },
+];
 
 export default function Ranking() {
+  const [sortBy, setSortBy] = useState('pointsThisMonth');
+
   return (
     <SafeArea>
-      <Text>Ranking screen</Text>
+      <Text style={styles.heading}>Ranking</Text>
+
+      <Picker
+        selectedValue={sortBy}
+        onValueChange={(value: string) => setSortBy(value)}
+      >
+        <Picker.Item label="Last 30 days" value="pointsThisMonth" />
+        <Picker.Item label="In total" value="pointsInTotal" />
+      </Picker>
+
+      <FlatList
+        data={MOCK_RANKING_DATA.sort((a, b) => b[sortBy] - a[sortBy])}
+        renderItem={({ item, index }) => {
+          return (
+            <View
+              style={
+                index === MOCK_RANKING_DATA.length - 1
+                  ? styles.listItemNoBorder
+                  : styles.listItem
+              }
+            >
+              <Text style={styles.number}>{index + 1}. </Text>
+              <Text>{item.name} </Text>
+              <Text style={styles.number}>{item[sortBy]} </Text>
+              {index === 0 && (
+                <Ionicons name="trophy" color="#f5e642" size={20} />
+              )}
+            </View>
+          );
+        }}
+      />
     </SafeArea>
   );
 }
+
+const styles = StyleSheet.create({
+  heading: {
+    fontSize: 24,
+    textAlign: 'center',
+    paddingVertical: 16,
+    backgroundColor: '#ddd',
+  },
+  listItem: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    textAlign: 'center',
+    padding: 10,
+    borderBottomWidth: 1,
+    borderColor: '#ddd',
+  },
+  listItemNoBorder: {
+    textAlign: 'center',
+    padding: 10,
+    flexDirection: 'row',
+    justifyContent: 'center',
+  },
+  number: {
+    fontWeight: 'bold',
+  },
+});
